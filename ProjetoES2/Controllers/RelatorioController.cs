ea63a35f554d95971fec43d947f58f22c5a57e23
guilhermealves1273 <guicardoso2002@hotmail.com>
2022-06-24 23:31:32 +0100

    hjvf

---
+++ ProjetoES2/Controllers/RelatorioController.cs
@@ -0,0 +1,104 @@
+﻿
+using Microsoft.AspNetCore.Mvc;
+using ProjetoES2.Context;
+using ProjetoES2.Entities;
+using ProjetoES2.Models;
+
+namespace ProjetoES2.Controllers;
+
+public class RelatorioController : Controller
+{
+    // GET
+    
+    private readonly MyDbContext _Context;
+
+    public RelatorioController()
+    {
+        _Context = new MyDbContext();
+    }
+    public IActionResult Index()
+    {
+        return View();
+    }
+    
+    public IActionResult Janeiro()
+    {
+       var tarefas = _Context.Tarefas.ToList().FindAll(x => x.DataInicio.Month == 1 && x.estado.Equals("Terminada") && x.IdUser==UserSession.idUtilizador);
+        return View(tarefas);
+    }
+    
+    public IActionResult Fevereiro()
+    {
+        var tarefas = _Context.Tarefas.ToList().FindAll(x => x.DataInicio.Month == 2 && x.estado.Equals("Terminada") && x.IdUser==UserSession.idUtilizador);
+        return View();
+    }
+    
+    public IActionResult Marco()
+    {
+        var tarefas = _Context.Tarefas.ToList().FindAll(x => x.DataInicio.Month == 3 && x.estado.Equals("Terminada") && x.IdUser==UserSession.idUtilizador);
+        return View();
+    }
+    
+    public IActionResult Abril()
+    {
+        var tarefas = _Context.Tarefas.ToList().FindAll(x => x.DataInicio.Month == 4 && x.estado.Equals("Terminada") && x.IdUser==UserSession.idUtilizador);
+        return View();
+    }
+    
+    public IActionResult Maio()
+    {
+        var tarefas = _Context.Tarefas.ToList().FindAll(x => x.DataInicio.Month == 5 && x.estado.Equals("Terminada") && x.IdUser==UserSession.idUtilizador);
+        return View();
+    }
+    
+    public IActionResult Junho()
+    {
+        
+       // var tarefas = _Context.Tarefas.ToList().FindAll(x => x.DataInicio.Month == 6 && x.estado.Equals("Terminada") && x.IdUser==UserSession.idUtilizador);
+       List<Tarefa> tarefas = new List<Tarefa>();
+       tarefas= _Context.Tarefas.ToList().FindAll(x => x.DataInicio.Month == 6 && x.estado.Equals("Terminada") && x.IdUser==UserSession.idUtilizador);
+       RelatorioModel model = new RelatorioModel();
+       model.tarefas = tarefas;
+       model.HorasTotais = 1;
+       model.PrecoTotal = 2;
+        return View(model);
+    }
+    
+    public IActionResult Julho()
+    {
+        var tarefas = _Context.Tarefas.ToList().FindAll(x => x.DataInicio.Month == 7 && x.estado.Equals("Terminada") && x.IdUser==UserSession.idUtilizador);
+        return View();
+    }
+    
+    public IActionResult Agosto()
+    {
+        var tarefas = _Context.Tarefas.ToList().FindAll(x => x.DataInicio.Month == 8 && x.estado.Equals("Terminada") && x.IdUser==UserSession.idUtilizador);
+        return View();
+    }
+    
+    public IActionResult Setembro()
+    {
+        var tarefas = _Context.Tarefas.ToList().FindAll(x => x.DataInicio.Month == 9 && x.estado.Equals("Terminada") && x.IdUser==UserSession.idUtilizador);
+        return View();
+    }
+    
+    public IActionResult Outubro()
+    {
+        var tarefas = _Context.Tarefas.ToList().FindAll(x => x.DataInicio.Month == 10 && x.estado.Equals("Terminada") && x.IdUser==UserSession.idUtilizador);
+        return View();
+    }
+    
+    public IActionResult Novembro()
+    {
+        var tarefas = _Context.Tarefas.ToList().FindAll(x => x.DataInicio.Month == 11 && x.estado.Equals("Terminada") && x.IdUser==UserSession.idUtilizador);
+        return View();
+    }
+    
+    public IActionResult Dezembro()
+    {
+        var tarefas = _Context.Tarefas.ToList().FindAll(x => x.DataInicio.Month == 12 && x.estado.Equals("Terminada") && x.IdUser==UserSession.idUtilizador);
+        return View();
+    }
+    
+
+}
---
+++ ProjetoES2/Models/RelatorioModel.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using ProjetoES2.Entities;
+
+namespace ProjetoES2.Models;
+
+public class RelatorioModel 
+{
+     public RelatorioModel()
+     {
+          tarefas = new List<Tarefa>();
+     }
+
+     public List<Tarefa> tarefas { get; set; }
+     
+     public float HorasTotais { get; set; }
+     
+     public float PrecoTotal { get; set; }
+     /*
+
+     public int IdTarefa { get; set; }
+        
+     
+     public string Descricao { get; set; } = null!;
+        
+        
+     
+     public DateTime DataInicio { get; set; }
+    
+     public DateTime? DataFim { get; set; }
+        
+    
+     public double PrecoHora { get; set; }
+        
+    
+     public int? IdProjeto { get; set; }
+        
+        
+     
+     public int IdUser { get; set; }
+        
+     
+     public string estado { get; set; }
+     
+     public String HorasTotais { get; set; }
+     
+     public String PrecoTotal { get; set; }
+     */
+   
+}
